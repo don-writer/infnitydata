7dd0504f7bf41e8a94a832fae997443a73a2d5d9
CG <cgordon.13@gmail.com>
2016-03-15 08:38:11 -0400

    Ordered unit data, but not exactly the same as the Perl script.  Need more investigation.  It might be fine as is.

--- scripts/clean_json.py
+++ scripts/clean_json.py
@@ -1,4 +1,5 @@
 import json, sys
+from collections import OrderedDict
 
 #print 'Number of arguments:', len(sys.argv), 'arguments.'
 #print 'Argument List:', str(sys.argv)
@@ -16,6 +17,18 @@ with open(filename, 'r') as f:
      
 #print data
 
+sort_order = [
+'army', 'sectorial', 'isc', 'name', 'sharedAva', 'image', 'optionSpecific', 'type', 'imp', 'irr', 
+'cube', 'hackable', 'mov', 'cc', 'bs', 'ph', 'wip', 'arm', 'bts', 'w', 'wtype', 's', 'ava', 
+'code', 'codename', 'cost', 'swc', 'profile', 'spec', 'bsw', 'ccw', 'independent', 'profiles', 'childs'
+]
+
+json_ordered = [OrderedDict(sorted(item.iteritems(), key=lambda (k, v): sort_order.index(k)))
+                    for item in data]
+
+
 print "Writing to: " + output_filename
 with open(output_filename, 'w') as outfile:
-    json.dump(data, outfile, indent=4, separators=(',', ': '))
+    json.dump(json_ordered, outfile, indent=4, separators=(',', ': '))
+    
+

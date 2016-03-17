5d1af62f3841ffec1a5a5b89101604ddbec59c8f
CG <cgordon.13@gmail.com>
2016-03-17 08:46:25 -0400

    Missed a script

---
+++ scripts/number_units.py
@@ -0,0 +1,30 @@
+import json, sys
+from collections import OrderedDict
+
+#print 'Number of arguments:', len(sys.argv), 'arguments.'
+#print 'Argument List:', str(sys.argv)
+
+if len(sys.argv) != 3:
+	print "Usage: number_units input_file start_index"
+	sys.exit(1)
+
+filename = sys.argv[1]
+index = int(sys.argv[2])
+
+print "Reading from: " + filename
+with open(filename, 'r') as f:
+     data = json.load(f, object_pairs_hook=OrderedDict)
+     
+#print data
+
+
+for i in range(len(data)):
+	index = index+1;
+	data[i].update({"index":index})
+
+
+print "Writing to: " + filename
+with open(filename, 'w') as outfile:
+    json.dump(data, outfile, indent=4, separators=(',', ': '))
+    
+

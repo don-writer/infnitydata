11d267c54fc472b42f42653eee2503d3b76d128b
Jonathan Polley <jonathan@ghostlords.com>
2016-04-12 20:45:17 -0700

    Change childs representation.

--- scripts/generate_name_id_mapping.py
+++ scripts/generate_name_id_mapping.py
@@ -3,52 +3,46 @@ import json, sys
 from collections import OrderedDict
 import codecs
 
-#print 'Number of arguments:', len(sys.argv), 'arguments.'
-#print 'Argument List:', str(sys.argv)
+"""Generate mappings of string names to integer ids.
+This is used for MayaNet to create an upgrade and compatibility mapping.
+"""
 
-if len(sys.argv) != 2:
-	print "Usage: clean_json input_file"
-	sys.exit(1)
+if len(sys.argv) != 1:
+    print 'Usage: ', sys.argv[0]
+    sys.exit(1)
 
-filename = sys.argv[1]
-
-print "Reading from: " + filename
-with open(filename, 'r') as f:
-     data = json.load(f)
-     
-#print data
-
-sort_order = [
-'comment', 'id', 'army', 'sectorial', 'isc', 'name', 'abbr', 'units', 'sharedAva', 'image', 
-'optionSpecific', 'type', 'imp', 'irr', 'cube', 'hackable', 'mov', 'cc', 'bs', 'ph', 'wip', 'arm',
-'bts', 'w', 'wtype', 's', 'ava', 'code', 'codename', 'cost', 'swc', 'profile', 'spec', 'bsw', 
-'ccw', 'independent', 'profiles', 'childs', 'hide', 'linkable'
+file_list = [
+    'alep_units.json',
+    'aria_units.json',
+    'comb_units.json',
+    'haqq_units.json',
+    'merc_units.json',
+    'noma_units.json',
+    'pano_units.json',
+    'toha_units.json',
+    'yuji_units.json'
 ]
 
+mappings = []
 
+for filename in file_list:
+    print 'Reading from: ' + filename
+    with open(filename, 'r') as f:
+        data = json.load(f)
 
-def unitsort(obj):
-	if isinstance(obj, list):
-		for i in range(len(obj)):
-			obj[i] = unitsort(obj[i])
-		return obj
-	elif isinstance(obj, dict):
-		for i in range(len(obj)):
-			obj[obj.keys()[i]] = unitsort(obj.values()[i])
-		obj =  OrderedDict(sorted(obj.iteritems(), key=lambda (k, v): sort_order.index(k)))
-		return obj
-	else:
-		return obj
+    for unit in data:
+        mapping = OrderedDict()
+        mapping['isc'] = unit['isc']
+        mapping['id'] = unit['id']
+        mapping['childs'] = OrderedDict()
+        for child in unit['childs']:
+            mapping['childs'][child['code']] = child['id']
+         
+        mappings.append(mapping)
 
-#json_ordered = [OrderedDict(sorted(item.iteritems(), key=lambda (k, v): sort_order.index(k)))
-#                    for item in data]
-
-#json_ordered = unitsort(data)
-
-data = unitsort(data)
-
-print "Writing to: " + filename
+filename = 'name_id_mapping.json'
+print 'Writing to: ' + filename
 with codecs.open(filename, 'w', encoding='utf-8') as outfile:
-    json.dump(data, outfile, indent=2, separators=(',', ': '), ensure_ascii=False)
+    json.dump(mappings, outfile, indent=2, separators=(',', ': '), ensure_ascii=False)
     
 

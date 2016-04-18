afaa0cd0655ad78259163900e41ad7a9d9484197
CG <cgordon.13@gmail.com>
2016-04-18 13:39:00 -0400

    Adding script that will rename code/codename to name and handle the instances where code == "Default" (where we need to use codename).  Delete code/codename from the childs after creating name

---
+++ scripts/rename_codes.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+import json, sys
+from collections import OrderedDict
+
+#print 'Number of arguments:', len(sys.argv), 'arguments.'
+#print 'Argument List:', str(sys.argv)
+
+if len(sys.argv) != 2:
+	print "Usage: number_units input_file"
+	sys.exit(1)
+
+filename = sys.argv[1]
+
+print "Reading from: " + filename
+with open(filename, 'r') as f:
+     data = json.load(f, object_pairs_hook=OrderedDict)
+     
+#print data
+
+
+for i in range(len(data)):
+	if data[i].has_key("childs"):
+		index = 1;
+		for j in range(len(data[i]["childs"])):
+			if data[i]["childs"][j]["code"] == "Default":
+				data[i]["childs"][j]["name"] = data[i]["childs"][j]["codename"] 
+			else:
+				data[i]["childs"][j]["name"] = data[i]["childs"][j]["code"] 
+			del data[i]["childs"][j]["code"] 
+			del data[i]["childs"][j]["codename"] 
+			
+
+print "Writing to: " + filename
+with open(filename, 'w') as outfile:
+    json.dump(data, outfile, indent=2, separators=(',', ': '))
+    
+
